169f588d0ddd768ddff6a7b481dcd80e0bb42d53
Md. Atiqul Islam <air.cse.pbn@gmail.com>
2023-09-07 09:06:13 +0600

    Add SMS receive and display a toast message showing the content

--- app/src/main/java/com/example/sms/MainActivity.java
+++ app/src/main/java/com/example/sms/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.sms;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkForSmsReceivePermissions();
+
+    }
+
+    void checkForSmsReceivePermissions(){
+        // Check if App already has permissions for receiving SMS
+        if(ContextCompat.checkSelfPermission(getBaseContext(), "android.permission.RECEIVE_SMS") == PackageManager.PERMISSION_GRANTED) {
+            // App has permissions to listen incoming SMS messages
+        } else {
+
+            // Request permissions from user
+            ActivityCompat.requestPermissions(this, new String[] {"android.permission.RECEIVE_SMS"}, 43391);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == 43391){
+            if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Log.d(this.getClass().getSimpleName(), "Sms Receive Permissions granted");
+            } else {
+                Log.d(this.getClass().getSimpleName(), "Sms Receive Permissions denied");
+            }
+        }
     }
 }
--- app/src/main/java/com/example/sms/SmsReceiver.java
+++ app/src/main/java/com/example/sms/SmsReceiver.java
@@ -1,4 +1,50 @@
 package com.example.sms;
 
-public class SmsReceiver {
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.Telephony;
+import android.telephony.SmsMessage;
+import android.widget.Toast;
+
+public class SmsReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+//        if (Telephony.Sms.Intents.SMS_RECEIVED_ACTION.equals(intent.getAction())) {
+//            for (SmsMessage smsMessage : Telephony.Sms.Intents.getMessagesFromIntent(intent)) {
+//                String messageBody = smsMessage.getMessageBody();
+//                String number = smsMessage.getOriginatingAddress();
+//
+//                Toast.makeText(context, "SMS Received: "+ messageBody + "from: " +
+//                        number, Toast.LENGTH_LONG).show();
+//
+//                Intent in = new Intent(context, MainActivity.class);
+//                in.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//                context.startActivity(in);
+//            }
+//        }
+
+        if (intent.getAction().equals("android.provider.Telephony.SMS_RECEIVED")) {
+            Bundle bundle=intent.getExtras();
+            SmsMessage[] chunks=null;
+            String number="";
+            String message="";
+            if(bundle!=null)
+            {
+                Object[] pdus=(Object[])bundle.get("pdus");
+                chunks=new SmsMessage[pdus.length];
+                for(int i=0;i<pdus.length;i++)
+                {
+                    chunks[i]=SmsMessage.createFromPdu((byte[])pdus[i]);
+                    number=chunks[i].getOriginatingAddress();
+                    message+=chunks[i].getMessageBody();
+                }
+            }
+
+            Toast.makeText(context,"SMS Received: "+ message+", From: "+number,
+                    Toast.LENGTH_LONG).show();
+
+        }
+    }
 }
